56fb1cabacf0406e13e4164b4189df26e0b95aae
Rakesh <pallalarakesh5@gmail.com>
2023-06-15 01:29:10 +0530

    Anagram Complete

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if (str1.length != str2.length){
+    return false;
+}
+for (i of str1){
+    let counter = 0;
+    for (j of str2){
+        if(i==j){
+            counter =1;
+            break;
+        }
+    }
+    if (counter==0){
+        return false;
+    }
+}
+return true;
 }
 
 module.exports = isAnagram;
